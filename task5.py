349d67c1aed17d7851c7b629cfdf69bb2a6760d7
SmahaElsana <samha@post.bgu.ac.il>
2023-06-26 10:58:18 +0300

    added python file

---
+++ task5.py
@@ -0,0 +1,15 @@
+from azure.storage.blob import BlobServiceClient
+
+connection_string = "DefaultEndpointsProtocol=https;AccountName=smahastorage1111;AccountKey=amgY8vqarqsLxK8o27Jbu7CIptQpuKW3y0GEu/vQk8ptJn+fI3acxoEqmIiSZ0ljmbnnPmBrVACd+AStRpArxQ==;EndpointSuffix=core.windows.net"
+
+
+blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+
+container_name = "container1"
+container_client = blob_service_client.create_container(container_name)
+
+blob_name = "myfirstblob"
+data = "Hello, Azure Blob Storage!"
+
+container_client.upload_blob(blob_name, data)
+
