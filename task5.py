6b3c6fc3ed827dc20139a5bb0e9dbdb8f7080be0
SmahaElsana <samha@post.bgu.ac.il>
2023-06-28 00:39:39 +0300

    mytask5

--- task5.py
+++ task5.py
@@ -1,16 +1,17 @@
-# from azure.identity import DefaultAzureCredential
-from azure.storage.blob import BlobServiceClient
+import os
+import uuid
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient, BlobType, PremiumPageBlobTier
 
 connection_string = "DefaultEndpointsProtocol=https;AccountName=smahastorage1111;AccountKey=amgY8vqarqsLxK8o27Jbu7CIptQpuKW3y0GEu/vQk8ptJn+fI3acxoEqmIiSZ0ljmbnnPmBrVACd+AStRpArxQ==;EndpointSuffix=core.windows.net"
-
 
 blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 
 container_name = "container1"
-container_client = blob_service_client.create_container(container_name)
+container_client = blob_service_client.get_container_client(container_name)
 
-blob_name = "myfirstblob"
+blob_name = "myfirstblob1.txt"
 data = "Hello, Azure Blob Storage!"
+data_bytes = data.encode("utf-8")
 
-container_client.upload_blob(blob_name, data)
-
+container_client.upload_blob(name=blob_name, data=data_bytes)
